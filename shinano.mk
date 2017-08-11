c199510c77f8b5ff440efa1327d3805ab9128b7e
Keita Espinoza <clouddarkgenesis@gmail.com>
2017-08-10 23:38:12 -0400

    shinano-common: Remove user nfcee_access.xml
    This signature is only used on build.type=user which is not our case.
    
    Change-Id: Ie08625dc438aad8c175f433c6309500afbb0e101

--- shinano.mk
+++ shinano.mk
@@ -123,7 +123,6 @@ PRODUCT_PACKAGES += \
     nfc_nci.pn54x.default
 
 PRODUCT_COPY_FILES += \
-    $(PLATFORM_PATH)/configs/nfc/nfcee_access.xml:system/etc/nfcee_access.xml \
     frameworks/native/data/etc/android.hardware.nfc.xml:system/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:system/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/com.android.nfc_extras.xml:system/etc/permissions/com.android.nfc_extras.xml \
